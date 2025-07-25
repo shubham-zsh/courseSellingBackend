a449cffe1e1d24d698bfe4f25991fbd664be3ee8
Shubham Borse <shubhamborse0911@gmail.com>
2025-07-25 17:17:48 +0530

    added admin route

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import jwt from 'jsonwebtoken';
 import express from 'express';
 import  userRouter  from './routes/user.js'
 import courseRouter from './routes/course.js';
+import adminRouter from './routes/admin.js';
 const app = express();
 
 app.use("/user", userRouter);
+app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
 
---
+++ routes/admin.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+function adminRouter() {
+
+}
+
+export default adminRouter;
