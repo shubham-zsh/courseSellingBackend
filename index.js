1791bcd5f6bee05b163837fc894b25b32d00c61a
Shubham Borse <shubhamborse0911@gmail.com>
2025-07-28 16:27:57 +0530

    included .env and mongo connection

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-import jwt from 'jsonwebtoken';
 import express from 'express';
-import  userRouter  from './routes/user.js'
+import userRouter from './routes/user.js'
 import courseRouter from './routes/course.js';
 import adminRouter from './routes/admin.js';
+import connectDB from './db.js';
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use("/user", userRouter);
-app.use("/admin", adminRouter);
-app.use("/course", courseRouter);
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
 
 
-app.listen(3000, () => {
+const startServer = async () => {
+    await  connectDB();
+app.listen(PORT, () => {
     console.log("express started");
-})
+});
+};
+
+startServer();
