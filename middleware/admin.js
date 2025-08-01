361b9135a1e63c4a189144f38cc912b13d697cd4
Shubham Borse <shubhamborse0911@gmail.com>
2025-08-01 18:03:44 +0530

    added admin middleware

---
+++ middleware/admin.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { jwtAdminSecret } from "../config";
+
+function adminMiddleware(req, res, next) {
+
+    const authHeader = req.headers.token;
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(401).json({ msg: "missing or invalid auth header" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, jwtAdminSecret);
+        req.adminId = decoded.id;
+        next();
+
+    } catch (err) {
+
+        console.error(err);
+        return res.status(401).json({ msg: "Invalid token" })
+    }
+}
+
+export default adminMiddleware;
